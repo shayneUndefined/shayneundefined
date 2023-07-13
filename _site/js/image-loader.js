79664ed45041874e2f00556b362e5b304eb824aa
stevex420 <shayne.valencia@icloud.com>
2023-07-12 23:58:15 -0700

    ds

---
+++ _site/js/image-loader.js
@@ -0,0 +1,14 @@
+function buildOutput() {
+	const output = document.querySelectorAll("section > span");
+	const urls = Array.from(document.querySelectorAll("section > span"));
+	const links = urls.map(x => x.innerText);
+	let arrayLength = links.length;
+	let i = 0;
+
+	output.forEach((span) => {
+		span.outerHTML = "<img src=\"" + links[i] + "\" height=\"600\" width=\"auto\">";
+		i++;}
+		)
+	}
+
+
